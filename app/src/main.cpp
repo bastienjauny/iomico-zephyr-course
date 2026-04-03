128eb9bd3a0c85778998b6bb38d51d5193aa5d0f
Bastien Jauny <bastien.jauny@smile.fr>
2026-04-03 15:26:33 +0200

    Lesson 3: added a custom menu in Kconfig

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,10 +1,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS 1000
-
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
-
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
@@ -12,10 +8,9 @@ int main(void)
     bool led_state = true;
 
     while (1) {
-        /* No LED on a esp32s3_rlcd_4_2, just toggle the boolean :) */
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
+        k_msleep(CONFIG_LED_BLINK_PERIOD_MS);
     }
     return 0;
 }
